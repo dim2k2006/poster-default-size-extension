c1d4bfbadf23984efe4096a034a693a3dc3bf3bd
Dmitry Malofeev <dim2k2006@yandex.ru>
2021-02-28 10:26:20 +0300

    Update sizes

--- src/index.tsx
+++ src/index.tsx
@@ -24,11 +24,13 @@ const sizes: SizeItem[] = [
   { orientation: 'portrait', width: 40, height: 50 },
   { orientation: 'portrait', width: 30, height: 40 },
   { orientation: 'portrait', width: 21, height: 30 },
+  { orientation: 'portrait', width: 10.5, height: 14.8 },
 
   { orientation: 'landscape', width: 70, height: 50 },
   { orientation: 'landscape', width: 50, height: 40 },
   { orientation: 'landscape', width: 40, height: 30 },
   { orientation: 'landscape', width: 30, height: 21 },
+  { orientation: 'landscape', width: 14.8, height: 10.5 },
 
   { orientation: 'square', width: 50, height: 50 },
   { orientation: 'square', width: 23, height: 23 },
